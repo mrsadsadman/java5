5efc638e24e70029a9bf663f0398f70f4166170f
mrsadsadman <76987782+mrsadsadman@users.noreply.github.com>
2026-02-26 10:17:44 +0700

    Delete assigment directory

--- assigment/src/main/java/poly/edu/assignment/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package poly.edu.assignment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import poly.edu.assignment.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
--- assigment/src/main/java/poly/edu/assignment/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package poly.edu.assignment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import poly.edu.assignment.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Role findByName(String name);
-}
